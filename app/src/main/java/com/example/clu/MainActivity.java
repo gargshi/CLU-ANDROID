5db07e48d492efe0e61cbb95f31d4e8a8b792f10
Shivam Garg <gargshivam34has@gmail.com>
2021-12-11 17:48:13 +0530

    Commit-17-47-11122021

--- app/src/main/java/com/example/clu/MainActivity.java
+++ app/src/main/java/com/example/clu/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        EditText editText = (EditText) findViewById(R.id.passwordEditText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
